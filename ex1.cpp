0daf9a66d4dc01169d973eb0c6749f1b7eff6aa3
adela-marin <adela.marin1321@gmail.com>
2024-04-17 17:23:59 +0300

    before testing

--- ex1.cpp
+++ ex1.cpp
@@ -1,8 +1,108 @@
 #include <iostream>
+#include "lablist.h"
 
 using namespace std;
 
-int main()
+class FlightBookingSystem {
+    public:
+        LinkedList<string> *flights;
+        int maxSeats = 5;
+        int currentSeats = 0;
+        int currentFlight = 0;
+        int currentSeat = 0;
+
+        void bookSeat(string passengerName) {
+            // if (currentSeats == 0) {
+            //     flights->addFirst(passengerName);
+            //     currentSeats++;
+            // } else if (currentSeats == maxSeats) {
+            //     flights->addLast(passengerName);
+            //     currentSeats = 1;
+            //     currentFlight++;
+            // } else {
+            //     flights->addLast(passengerName);
+            //     currentSeats++;
+            // }
+
+            // if (currentFlight == 0) {
+            //     currentFlight++;
+            // }
+
+            if (currentSeats == maxSeats) {
+                flights->addLast(passengerName);
+                currentSeats = 1;
+                currentFlight++;
+            } else {
+                flights->addLast(passengerName);
+                currentSeats++;
+            }
+
+        }
+
+        void cancelBooking(int seatIndex) {
+            Node<string> *p = flights->pfirst;
+            Node<string> *prev = NULL;
+            int i = 0;
+            while (p != NULL) {
+                if (i == seatIndex) {
+                    if (prev == NULL) {
+                        flights->pfirst = p->next;
+                    } else {
+                        prev->next = p->next;
+                    }
+                    delete p;
+                    currentSeats--;
+                    return;
+                }
+                prev = p;
+                p = p->next;
+                i++;
+            }
+        }
+
+
+        void displaySystem() {
+            Node<string> *p = flights->pfirst;
+            while (p != NULL) {
+                cout << p->info << endl;
+                p = p->next;
+            }
+        }
+
+        string getPassenger(int seatIndex) {
+            Node<string> *p = flights->pfirst;
+            int i = 0;
+            while (p != NULL) {
+                if (i == seatIndex) {
+                    return p->info;
+                }
+                p = p->next;
+                i++;
+            }
+            return "";
+        }
+
+        FlightBookingSystem() {
+            flights = new LinkedList<string>();
+        }
+
+        ~FlightBookingSystem() {
+            delete flights;
+        }
+
+};
+
+int main() 
 {
-	
+    FlightBookingSystem *zboara = new FlightBookingSystem();
+    zboara->bookSeat("Sergiu");
+    zboara->bookSeat("Ana");
+    zboara->displaySystem();
+    cout << zboara->getPassenger(0) << endl;
+    zboara->cancelBooking(0);
+    zboara->displaySystem();
+    delete zboara;
+    return 0;
 }
+
+
