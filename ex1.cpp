ead8041d3a55751e82ee9a33ff5c8d999ebcd866
adela-marin <adela.marin1321@gmail.com>
2024-04-19 14:45:11 +0300

    input from keyboard

--- ex1.cpp
+++ ex1.cpp
@@ -95,13 +95,51 @@ class FlightBookingSystem {
 int main() 
 {
     FlightBookingSystem *zboara = new FlightBookingSystem();
-    zboara->bookSeat("Sergiu");
-    zboara->bookSeat("Ana");
-    zboara->displaySystem();
-    cout << zboara->getPassenger(0) << endl;
-    zboara->cancelBooking(0);
-    zboara->displaySystem();
-    delete zboara;
+    // zboara->bookSeat("Sergiu");
+    // zboara->bookSeat("Ana");
+    // zboara->displaySystem();
+    // cout << zboara->getPassenger(0) << endl;
+    // zboara->cancelBooking(0);
+    // zboara->displaySystem();
+    // delete zboara;
+
+    // test the flight booking system with data from the keyboard
+
+    int option;
+    string passengerName;
+    int seatIndex;
+
+    while (true) {
+        cout << "1. Book a seat" << endl;
+        cout << "2. Cancel booking" << endl;
+        cout << "3. Display system" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Option: ";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                cout << "Passenger name: ";
+                cin >> passengerName;
+                zboara->bookSeat(passengerName);
+                break;
+            case 2:
+                cout << "Seat index: ";
+                cin >> seatIndex;
+                zboara->cancelBooking(seatIndex);
+                break;
+            case 3:
+                zboara->displaySystem();
+                break;
+            case 4:
+                delete zboara;
+                return 0;
+            default:
+                cout << "Invalid option" << endl;
+        }
+    }
+
+    
     return 0;
 }
 
